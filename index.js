d20563d8c77f9ba261f823575fc96cf123955db6
Kalyan Singh <95122829+KalyanSingh05@users.noreply.github.com>
2024-07-07 12:25:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = 4000;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://your-server-a.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://server-a-beta.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
